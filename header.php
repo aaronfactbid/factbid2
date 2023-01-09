3770ec9a7e6ef02aacf047cf6f70ee0952aa8427
aaronfactbid <aaron@factbid.org>
2023-01-09 10:44:58 -0300

    add cache control to header

--- header.php
+++ header.php
@@ -8,6 +8,7 @@ $site_url =  "https://".$_SERVER['SERVER_NAME'];
 	
 	/** DB Connection file */
 	require_once('config.php');
+	header("Cache-Control: public, max-age=600");
 ?>
 <!DOCTYPE html>
 <html>
