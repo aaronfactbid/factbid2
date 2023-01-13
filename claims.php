80db85b485fd1becc3ee348e5b1454f04c850b21
aaronfactbid <aaron@factbid.org>
2023-01-13 12:17:16 -0300

    fix the text on the claims page

--- claims.php
+++ claims.php
@@ -3,12 +3,7 @@
 
    <div class="bidswrapper">
    	<p>
-   	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-   	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-   	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-   	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-   	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-   	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+   	Whistleblowers post evidence and their donation instructions in a tweet with the hashtag they are claiming plus #factbidclaim.  Below are the tweets from whistleblowers.
    </p></div>
 
    <?php
@@ -36,7 +31,7 @@
 <table>
 	<thead>
 	 <tr>
-    <th>Author</th>
+    <th>Whistleblower</th>
     <th>Date</th>
 </tr>
 	</thead>
@@ -44,7 +39,7 @@
 <?php while($row = $claimResult->fetch_assoc()) {  ?>
 	
 	<tr>
-		<td>@<a href="#"><?php echo  $row['author']; ?></a></td>
+		<td>@<a href="https://twitter.com/<?php echo  $row['author_username']; ?>/status/<?php echo  $row['id_twitter']; ?>"><?php echo  $row['author_username']; ?></a></td>
 		<td><?php echo '<script>var date = new Date(' . $row['created_int'] .' * 1000); document.write(date.toLocaleString());</script>';?></td>
 	</tr>
 
