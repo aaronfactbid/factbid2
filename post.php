b2c0b566690972761728d29070b1ef65d5865bf1
aaronfactbid <100711735+aaronfactbid@users.noreply.github.com>
2023-08-20 12:02:17 -0300

    fix post.php

--- post.php
+++ post.php
@@ -1,6 +1,13 @@
 <?php
    require_once('header.php'); 
    $current_hashtag = $_GET['hashtag'];
-   header("Location: " . tweet_bid($row_ht['hashtag'],$row_ht['id_twitter'],$row_ht['author_username'],$row_ht['template'],$row_ht['tweet_url']), true, 301);
-   exit();
+$sql = "SELECT *,UNIX_TIMESTAMP(created_ts) AS created_int FROM hashtag WHERE `hashtag`='" . $current_hashtag . "' LIMIT 1;";
+$result_ht = mysqli_query($conn, $sql);
+
+if(mysqli_num_rows($result_ht) > 0) { 
+	$row_ht = $result_ht->fetch_assoc();
+	$id_hashtag = $row_ht['id_hashtag'];
+	header("Location: " . tweet_bid($row_ht['hashtag'],$row_ht['id_twitter'],$row_ht['author_username'],$row_ht['template'],$row_ht['tweet_url']), true, 301);
+	exit();
+}
 ?>
